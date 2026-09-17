cc220ee2e60fa5cf57b1171cd86eeb1925dc1145
dechtel <muhammad.aljuburi2007@gmail.com>
2026-09-18 01:42:34 +0300

    Refactor tui setup and implement move functionality
    Refactor tui initialization for better readability and add move function to handle character movement.

--- termkillship.py
+++ termkillship.py
@@ -1,6 +1,20 @@
 # Use lists of characters so we can modify individual positions
 
-tui = [list("333333333"), list("000000000"), list("000000000"), list("000000000"), list("000000000"), list("000000000"), list("000000000"),list("000000000"),list("000000000"),list("000000000"),list("000000000"),list("000000000"), list("000020000")]
+tui = [
+    list("333333333"),
+    list("000000000"),
+    list("000000000"),
+    list("000000000"),
+    list("000000000"),
+    list("000000000"),
+    list("000000000"),
+    list("000000000"),
+    list("000000000"),
+    list("000000000"),
+    list("000000000"),
+    list("000000000"),
+    list("000020000")]
+
 
 def redraw(tui):
     for row in tui:
@@ -39,6 +53,32 @@ def fire(tui,yes=True):
             y1 = y - 1
             if y1 >= 0:          # avoid going out of bounds
                 tui[y1][x] = "1"
-#def anti_fire(tui) :
-#  for y in 
 
+#def anti_fire(tui) :
+#  for y in
+
+def move(tui,cmd=""):
+    _x,_y= loca(tui,"2")
+    if  cmd =="a" and _x == 0  or cmd =="d" and _x == len(tui[_y])-1 :
+        pass
+
+    elif cmd == "a" :
+        tui[_y][_x-1]="2"
+        tui[_y][_x] = "0"
+    elif cmd == "d" :
+        tui[_y][_x+1]="2"
+        tui[_y][_x] = "0"
+    else:
+        pass
+
+
+
+def gedraw(canva,tui,p0,p1,p2,p3,p4) :
+  y = 0
+  for yy in tui :
+    x = 0
+    for xx in yy :
+      imgs = {"0":p0, "1":p1 ,"2":p2,"3":p3 , "4":p4}
+      canva.create_image( x,y,anchor="nw",image = imgs[xx] , tags = "fg")
+      x += 54
+    y += 54
