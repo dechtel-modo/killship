50cc3990e1ff08d66f80c29313f076afcaf489bf
dechtel <muhammad.aljuburi2007@gmail.com>
2026-09-18 01:43:09 +0300

    Update GUI layout and functionality in gui.py

--- gui.py
+++ gui.py
@@ -1,50 +1,66 @@
 import tkinter as t
+
 import termkillship as s
 
 root= t.Tk()
-root.geometry("480x768")
+root.geometry("480x780")
 tui = s.tui
 
 bg = t.PhotoImage(file="b.png")
-p0 = t.PhotoImage(file="00.png")
-p1 = t.PhotoImage(file="11.png")
-p2 = t.PhotoImage(file="22.png")
-p3 = t.PhotoImage(file="33.png")
-p4 = t.PhotoImage(file="44.png")
+p0 = t.PhotoImage(file="0.png")
+p1 = t.PhotoImage(file="1.png")
+p2 = t.PhotoImage(file="2.png")
+p3 = t.PhotoImage(file="3.png")
+p4 = t.PhotoImage(file="4.png")
 
 
-def gedraw(canva,tui,p0,p1,p2,p3,p4) :
-  y = 0
-  for yy in tui :
-    x = 0
-    for xx in yy :
-      imgs = {"0":p0, "1":p1 ,"2":p2,"3":p3 , "4":p4}
-      canva.create_image(x,y,anchor="nw",image = imgs[xx] , tags = "fg")
-      x += 48
-    y += 48
-
-
-
-canvas1 = t.Canvas( root, width = 480,height = 768)
+canvas1 = t.Canvas( root, width = 480,height = 780)
 root.resizable(0,0)
 canvas1.pack(fill = "both", expand = False)
 canvas1.create_image( 0, 0, image = bg, anchor = "nw")
+
+
+
+
 tt = False
-t = 0
+te = 0
+
+def dir_d():
+  global tui  # noqa: PLW0602
+  s.move(tui,"d")
+
+def dir_a():
+  global tui  # noqa: PLW0602
+  s.move(tui,"a")
+
+dira = t.Button(text="«<",width=225,height=40,command=dir_a)
+dird = t.Button(text=">»",width=225,height=40,command=dir_d)
+
+
+canvas1.create_window(120,757,width=225,height=40,window=dira)
+canvas1.create_window(360,757,width=225,height=40,window=dird)
+
+
 def _loop():
-  global tui ,p0 ,p1 ,p2 ,p3 ,p4 ,t,tt
-  t += 1
-  
+  global tui, p0,p1,p2,p3,p4,te,tt  # noqa: PLW0602
+  te += 1  
   canvas1.delete("fg")
-  canvas1.create_image( 0, 0, image = bg, anchor = "nw")
-  if t % 7 == 0 :
+  if te == 31 :
     tt = True
+    te = 0
   s.fire(tui,tt)
   tt = False
-  gedraw(canvas1,tui,p0,p1,p2,p3,p4)
-  root.after(400,_loop)
+  #root.bind('<KP_Left>',dir_a)
+  #root.bind('<KeyPress_Right>',dir_d)
+  #root.bind('<Return>',lambda:"p" in print("io"))
+  s.gedraw(canvas1,tui,p0,p1,p2,p3,p4)
   
-root.after(1000,_loop)
+  
+  
+  root.after(25,_loop)
 
 
+
+
+root.after(100,_loop)
 root.mainloop()
